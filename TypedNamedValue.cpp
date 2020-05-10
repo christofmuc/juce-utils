7fe073c04516d82e50430137e6fe8381bf86bec9
cruc <christof.ruch@gmx.de>
2020-05-10 14:27:53 +0200

    Big refactoring - change TypedNamedValue into a proper class from a struct. You should really stay away from structs from the start, every time I use them I end up here.

---
+++ TypedNamedValue.cpp
@@ -0,0 +1,126 @@
+/*
+   Copyright (c) 2020 Christof Ruch. All rights reserved.
+
+   Dual licensed: Distributed under Affero GPL license by default, an MIT license is available for purchase
+*/
+
+#include "TypedNamedValue.h"
+
+TypedNamedValue::TypedNamedValue(String const &name, String const &sectionName, bool defaultValue) : name_(name), sectionName_(sectionName), valueType_(ValueType::Bool)
+{
+	value_ = Value(defaultValue);
+	minValue_ = 0;
+	maxValue_ = 1;
+	enabled_ = true;
+}
+
+TypedNamedValue::TypedNamedValue(String const &name, String const &sectionName, String const &defaultValue, int maxLength) : name_(name), sectionName_(sectionName), valueType_(ValueType::String)
+{
+	value_ = Value(defaultValue);
+	minValue_ = 0;
+	maxValue_ = maxLength;
+	enabled_ = true;
+}
+
+TypedNamedValue::TypedNamedValue(String const &name, String const &sectionName, int defaultValue, int minValue, int maxValue) : name_(name), sectionName_(sectionName), valueType_(ValueType::Integer)
+{
+	value_ = Value(defaultValue);
+	minValue_ = minValue;
+	maxValue_ = maxValue;
+	enabled_ = true;
+}
+
+TypedNamedValue::TypedNamedValue(String const &name, String const &sectionName, int defaultValue, std::map<int, std::string> const &lookup) : name_(name), sectionName_(sectionName), valueType_(ValueType::Lookup)
+{
+	setLookup(lookup);
+	value_ = Value(defaultValue);
+	enabled_ = true;
+}
+
+juce::Value &TypedNamedValue::value()
+{
+	return value_;
+}
+
+juce::String TypedNamedValue::name() const
+{
+	return name_;
+}
+
+juce::String TypedNamedValue::sectionName() const
+{
+	return sectionName_;
+}
+
+ValueType TypedNamedValue::valueType() const
+{
+	return valueType_;
+}
+
+int TypedNamedValue::minValue() const
+{
+	return minValue_;
+}
+
+int TypedNamedValue::maxValue() const
+{
+	return maxValue_;
+}
+
+bool TypedNamedValue::enabled() const
+{
+	return enabled_;
+}
+
+std::map<int, std::string> TypedNamedValue::lookup() const
+{
+	return lookup_;
+}
+
+int TypedNamedValue::indexOfValue(std::string const &text) const
+{
+	for (auto const &d : lookup_) {
+		if (d.second == text) {
+			return d.first;
+		}
+	}
+	return 0;
+}
+
+std::string TypedNamedValue::lookupValue() 
+{
+	return lookup_[int(value_.getValue())];
+}
+
+void TypedNamedValue::setLookup(std::map<int, std::string> const &newLookup)
+{
+	lookup_ = newLookup;
+	if (lookup_.empty()) {
+		minValue_ = maxValue_ = 0;
+	}
+	else {
+		minValue_ = lookup_.begin()->first;
+		maxValue_ = lookup_.rbegin()->first;
+	}
+}
+
+std::shared_ptr<TypedNamedValue> TypedNamedValueSet::typedNamedValueByName(std::string const &name)
+{
+	for (auto tnv : *this) {
+		if (tnv->name().toStdString() == name) {
+			return tnv;
+		}
+	}
+	jassertfalse;
+	return {};
+}
+
+juce::Value &TypedNamedValueSet::valueByName(std::string const &name)
+{
+	for (auto tnv : *this) {
+		if (tnv->name().toStdString() == name) {
+			return tnv->value();
+		}
+	}
+	throw std::runtime_error("Illegal argument - key does not exist");
+}
--- TypedNamedValue.h
+++ TypedNamedValue.h
@@ -1,3 +1,9 @@
+/*
+   Copyright (c) 2020 Christof Ruch. All rights reserved.
+
+   Dual licensed: Distributed under Affero GPL license by default, an MIT license is available for purchase
+*/
+
 #pragma once
 
 #include "JuceHeader.h"
@@ -9,14 +15,47 @@ enum class ValueType {
 	Lookup
 };
 
-struct  TypedNamedValue {
-	String name;
-	String sectionName;
-	Value value;
-	ValueType valueType;
-	int minValue;
-	int maxValue;
-	std::map<int, std::string> lookup;
-	bool enabled;
+class TypedNamedValue {
+public:
+	//! Construct a Bool type
+	TypedNamedValue(String const &name, String const &sectionName, bool defaultValue);
+	//! Construct a String type (edit field)
+	TypedNamedValue(String const &name, String const &sectionName, String const &defaultValue, int maxLength);
+	//! Construct an Integer type
+	TypedNamedValue(String const &name, String const &sectionName, int defaultValue, int minValue, int maxValue);
+	//! Construct a lookup value type
+	TypedNamedValue(String const &name, String const &sectionName, int defaultValue, std::map<int, std::string> const &lookup);
+
+	virtual ~TypedNamedValue() = default;
+
+	Value &value();
+	String name() const;
+	String sectionName() const;
+	ValueType valueType() const;
+	int minValue() const;
+	int maxValue() const;
+	bool enabled() const;
+	std::map<int, std::string> lookup() const;
+
+	// For lookups only
+	int indexOfValue(std::string const &text) const;  //TODO - suboptimal linear search
+	std::string lookupValue();
+	void setLookup(std::map<int, std::string> const &newLookup);
+
+protected:
+	String name_;
+	String sectionName_;
+	Value value_;
+	ValueType valueType_;
+	int minValue_;
+	int maxValue_;
+	std::map<int, std::string> lookup_;
+	bool enabled_;
+};
+
+class TypedNamedValueSet : public std::vector<std::shared_ptr<TypedNamedValue>> {
+public:
+	std::shared_ptr<TypedNamedValue> typedNamedValueByName(std::string const &name); //TODO - suboptimal linear search
+	Value &valueByName(std::string const &name); //TODO - suboptimal linear search
 };
 
