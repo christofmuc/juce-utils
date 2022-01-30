27171ac2683033805314a11d1e6c9e8ea507431a
Christof Ruch <christof.ruch@gmx.de>
2022-01-30 17:49:55 +0100

    [i18n] Add changes required for the I18NHelper to work on Mac. This is pretty rediculous, effectively these are three different implementations now. How to test?

--- I18NHelper.cpp
+++ I18NHelper.cpp
@@ -28,6 +28,8 @@
 
 #include "Logger.h"
 
+#include <stdlib.h>
+
 #ifndef USE_GETTEXT_TEXT_DOMAIN
 #define USE_GETTEXT_TEXT_DOMAIN "juceutils"
 #endif
@@ -38,7 +40,13 @@ File gLocalePath;
 
 juce::String U8(const char* translatedString)
 {
-	return juce::String::fromUTF8(translatedString);
+    if (translatedString != nullptr) {
+        return juce::String::fromUTF8(translatedString);
+    }
+    else {
+        jassertfalse;
+        return "<missing_trans>";
+    }
 }
 
 void globalSetupLocale()
@@ -57,8 +65,9 @@ void globalSetupLocale()
 	}
 	auto result = bindtextdomain(USE_GETTEXT_TEXT_DOMAIN, gLocalePath.getFullPathName().getCharPointer());
 	SimpleLogger::instance()->postMessage("Bindtext domain gave us " + String(result));
-#if defined(WIN32) || defined(__APPLE__)
+#if defined(WIN32) || defined(__APPLE__)
 	auto displayLocale = juce::SystemStats::getDisplayLanguage().getCharPointer();
+    bind_textdomain_codeset(USE_GETTEXT_TEXT_DOMAIN, "utf-8");
 #else
     auto displayLocale = ::setlocale (LC_MESSAGES, "");
 #endif
@@ -69,13 +78,17 @@ void switchDisplayLanguage(const char* languageID)
 {
 	const char* localeToSet;
 
-#ifdef WIN32
+#if defined(WIN32) || defined(__APPLE__)
 	// Windows famously ignores the setlocale, but rather insists on reading the environment. Additionally, Windows uses de-DE and not de_DE like Posix.
 	// We discard country specific language at this point to keep it simple
 	std::string cleanupId(languageID);
 	std::replace(cleanupId.begin(), cleanupId.end(), '-', '_');
 	localeToSet = cleanupId.c_str();
+#ifdef WIN32
 	_putenv_s("LC_ALL", localeToSet);
+#else
+    setenv("LANG", localeToSet, 1);
+#endif
 #else
 	::setlocale(LC_MESSAGES, languageID);
     localeToSet = languageID;
