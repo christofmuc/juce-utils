73274fe14867fbcb7583242b41d713ca5e892a14
Christof Ruch <christof.ruch@gmx.de>
2022-02-02 15:25:38 +0100

    [fix] Fix mac and Linux build.

--- I18NHelper.cpp
+++ I18NHelper.cpp
@@ -66,10 +66,10 @@ void globalSetupLocale()
 	auto result = bindtextdomain(USE_GETTEXT_TEXT_DOMAIN, gLocalePath.getFullPathName().getCharPointer());
 	SimpleLogger::instance()->postMessage("Bindtext domain gave us " + String(result));
 #if defined(WIN32) || defined(__APPLE__)
-	std::string displayLocale = juce::SystemStats::getDisplayLanguage().getCharPointer();
+	std::string displayLocale = juce::SystemStats::getDisplayLanguage().toStdString();
     bind_textdomain_codeset(USE_GETTEXT_TEXT_DOMAIN, "utf-8");
 #else
-    auto displayLocale = ::setlocale (LC_MESSAGES, "");
+	std::string displayLocale = ::setlocale (LC_MESSAGES, "");
 #endif
 	switchDisplayLanguage(displayLocale.c_str());
 }
